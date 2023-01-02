29d88394cf191e4ef2c3387ce3036e8ab02e8be8
phyo-pyae <phyopyaenyeinchanuit@gmail.com>
2023-01-02 16:04:40 +0630

    adding some aditional routes

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,4 +1,8 @@
+import 'dart:js';
+
+import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_item.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../features/products_list/products_list_screen.dart';
 
@@ -12,7 +16,12 @@ final goRouter = GoRouter(
       routes: [
         GoRoute(
           path: 'cart',
-          builder: (context, state) => const ShoppingCartScreen(),
+          // builder: (context, state) => const ShoppingCartScreen(),
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: ShoppingCartScreen(),
+          ),
         ),
       ],
     ),
